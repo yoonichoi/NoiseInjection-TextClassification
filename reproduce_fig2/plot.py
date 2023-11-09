1c48c1423e3db84b6ede1b67bdeb049771e152c7
yoonichoi <yooni.jy.choi@gmail.com>
2023-11-09 02:08:46 +0000

    code for plotting figure

---
+++ reproduce_fig2/plot.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import glob
+
+folder_path = 'reproduce_fig2/outputs'
+file_paths = []
+for sd in range(5):
+    file_paths.extend(glob.glob(f'{folder_path}/result_{sd}.csv'))
+
+dfs = [pd.read_csv(file_path, sep=' ', names=['Dataset', 'Percentage', 'Original', 'EDA', 'AEDA']) for file_path in file_paths]
+aggregated_df = pd.concat(dfs, ignore_index=True)
+
+average_df = aggregated_df.groupby(['Dataset', 'Percentage']).mean().reset_index()
+
+average_df.to_csv(f'{folder_path}/aggregated_results.csv', index=False)
+
+datasets = average_df['Dataset'].unique()
+
+fig, axes = plt.subplots(1, len(datasets), figsize=(15, 5), sharey=True)
+
+for i, dataset in enumerate(datasets):
+    dataset_df = average_df[average_df['Dataset'] == dataset]
+
+    axes[i].plot(dataset_df['Percentage'], dataset_df['Original'], label=f'Original')
+    axes[i].plot(dataset_df['Percentage'], dataset_df['EDA'], label=f'EDA')
+    axes[i].plot(dataset_df['Percentage'], dataset_df['AEDA'], label=f'AEDA')
+
+    axes[i].set_xlabel('Percentage of Data')
+    axes[i].set_ylabel('Accuracy')
+    axes[i].set_title(f'{dataset}')
+    axes[i].legend()
+
+# Save the figure with all subplots
+plt.savefig(f'{folder_path}/accuracy_trend.png')
