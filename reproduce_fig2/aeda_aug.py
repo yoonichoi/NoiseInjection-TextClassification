e8ac973cfa6299024b5b30922d700a1bf4657ca5
nikhilkandukuri <nikhilkand@gmail.com>
2023-11-27 20:56:14 -0500

    Restoring original reproduce folder

--- reproduce_fig2/aeda_aug.py
+++ reproduce_fig2/aeda_aug.py
@@ -1,28 +1,20 @@
-# AEDA: An Easier Data Augmentation Technique for Text classification
-# Akbar Karimi, Leonardo Rossi, Andrea Prati
+# Easy data augmentation techniques for text classification
+# Jason Wei, Chengyu Huang, Yifang Wei, Fei Xing, Kai Zou
 
 import random
-from random import shuffle
 
 random.seed(0)
 
 PUNCTUATIONS = ['.', ',', '!', '?', ';', ':']
 DATASETS = ['cr', 'sst2', 'subj', 'pc', 'trec']
-# DATASETS = ['data/train/pc']
 NUM_AUGS = [1, 2, 4, 8]
-# NUM_AUGS = [3]
-ADD_RATIO = 0.3
-# LENGTHS = [1,2,3,4,5,6,7,8,9,10]	# length of random numbers/alphabets
+PUNC_RATIO = 0.3
 
-####################################
-###########		AEDA	############
-####################################
-
-# Insert punction words into a given sentence with the given ratio "add_ratio"
-def insert_punctuation_marks(sentence, add_ratio=ADD_RATIO):
+# Insert punction words into a given sentence with the given ratio "punc_ratio"
+def insert_punctuation_marks(sentence, punc_ratio=PUNC_RATIO):
 	words = sentence.split(' ')
 	new_line = []
-	q = random.randint(1, int(add_ratio * len(words) + 1))
+	q = random.randint(1, int(punc_ratio * len(words) + 1))
 	qs = random.sample(range(0, len(words)), q)
 
 	for j, word in enumerate(words):
@@ -34,101 +26,8 @@ def insert_punctuation_marks(sentence, add_ratio=ADD_RATIO):
 	new_line = ' '.join(new_line)
 	return new_line
 
-####################################
-######## Adding Numbers	############
-####################################
 
-def generate_random_number(length):
-	# given length, generate a random number of that length
-	number = ''
-	for i in range(length):
-		number += str(random.randint(0, 9))
-	return number
-
-# Insert random number words of specified length into a given sentence with the given ratio "add_ratio"
-def insert_numbers(sentence, length, add_ratio=ADD_RATIO):
-	words = sentence.split(' ')
-	new_line = []
-	q = random.randint(1, int(add_ratio * len(words) + 1))	# number of numbers to add
-	qs = random.sample(range(0, len(words)), q)	# indices to add numbers to
-
-	for j, word in enumerate(words):
-		if j in qs:
-			new_line.append(generate_random_number(length))
-			new_line.append(word)
-		else:
-			new_line.append(word)
-	new_line = ' '.join(new_line)
-	return new_line
-
-
-####################################
-######## Adding Alphabets	########
-####################################
-
-def generate_random_alphabets(length):
-	# given length, generate a random alphabet of that length
-	alphabet = ''
-	for i in range(length):
-		alphabet += chr(random.randint(97, 122))
-	return alphabet
-
-# Insert random number words of specified length into a given sentence with the given ratio "add_ratio"
-def insert_alphabets(sentence, length, add_ratio=ADD_RATIO):
-	words = sentence.split(' ')
-	new_line = []
-	q = random.randint(1, int(add_ratio * len(words) + 1))	# number of alphabets to add
-	qs = random.sample(range(0, len(words)), q)	# indices to add alphabets to
-
-	for j, word in enumerate(words):
-		if j in qs:
-			new_line.append(generate_random_alphabets(length))
-			new_line.append(word)
-		else:
-			new_line.append(word)
-	new_line = ' '.join(new_line)
-	return new_line
-
-
-def aeda_3(sentence, num_aug=9):
-
-	augmented_sentences = []
-	num_new_per_technique = int(num_aug / 4) + 1
-
-	# punc
-	for _ in range(num_new_per_technique):
-		augmented_sentence = insert_punctuation_marks(sentence)
-		augmented_sentences.append(augmented_sentence)
-
-	# char
-	for _ in range(num_new_per_technique):
-		augmented_sentence = insert_numbers(sentence, random.randint(1,10))
-		augmented_sentences.append(augmented_sentence)
-
-	# num
-	for _ in range(num_new_per_technique):
-		augmented_sentence = insert_alphabets(sentence, random.randint(1,10))
-		augmented_sentences.append(augmented_sentence)
-
-	shuffle(augmented_sentences)
-
-	# trim so that we have the desired number of augmented sentences
-	if num_aug >= 1:
-		augmented_sentences = augmented_sentences[:num_aug]
-	else:
-		keep_prob = num_aug / len(augmented_sentences)
-		augmented_sentences = [s for s in augmented_sentences if random.uniform(0, 1) < keep_prob]
-
-	# append the original sentence
-	augmented_sentences.append(sentence)
-
-	return augmented_sentences
-
-
-
-
-def main(dataset, aug_type):
-	outputfile = None
+def main(dataset):
 	for aug in NUM_AUGS:
 		data_aug = []
 		with open(dataset + '/train.txt', 'r') as train_orig:
@@ -137,24 +36,15 @@ def main(dataset, aug_type):
 				label = line1[0]
 				sentence = line1[1]
 				for i in range(aug):
-					if aug_type == 'punctuation':
-						outputfile = 'train_punc_aug'
-						sentence_aug = insert_punctuation_marks(sentence)
-					elif aug_type == 'numbers':
-						outputfile = 'train_num_aug'
-						sentence_aug = insert_numbers(sentence, random.randint(1,10))
-					else:
-						outputfile = 'train_alpha_aug'
-						sentence_aug = insert_alphabets(sentence, random.randint(1,10))
+					sentence_aug = insert_punctuation_marks(sentence)
 					line_aug = '\t'.join([label, sentence_aug])
 					data_aug.append(line_aug)
 				data_aug.append(line)
 
-		with open(f"{dataset}/{outputfile}_{aug}.txt", 'w') as train_orig_plus_augs:
+		with open(dataset + '/train_orig_plus_augs_' + str(aug) + '.txt', 'w') as train_orig_plus_augs:
 			train_orig_plus_augs.writelines(data_aug)
 
 
 if __name__ == "__main__":
-	aug_type = 'alphabets'	# 'punctuation', 'numbers', 'alphabets'
 	for dataset in DATASETS:
-		main(dataset, aug_type)
+		main(dataset)
--- reproduce_fig2/data_process.py
+++ reproduce_fig2/data_process.py
@@ -15,10 +15,8 @@ if __name__ == "__main__":
 
 		# AEDA augmentation
 		train_aeda = dataset_folder + '/train_aeda.txt'
-		train_aeda2 = dataset_folder + '/train_aeda_hybrid.txt'
-		gen_aeda_aug(train_orig,train_aeda)
-		gen_hybrid_aeda_aug(train_orig, train_aeda2)	# creates train+aeda
+		gen_aeda_aug(train_orig, train_aeda)	# creates train+aeda
 
-		# #generate the vocab dictionary
+		#generate the vocab dictionary
 		word2vec_pickle = dataset_folder + '/word2vec.pkl' # don't want to load the huge pickle every time, so just save the words that are actually used into a smaller dictionary
 		gen_vocab_dicts(dataset_folder, word2vec_pickle, huge_word2vec)
