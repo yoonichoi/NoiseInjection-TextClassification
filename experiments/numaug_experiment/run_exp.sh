abe961038ba2bd79d0deb76cb844d1261d9d623a
yoonichoi <yooni.jy.choi@gmail.com>
2023-11-27 22:52:42 -0500

    edit run_exp

--- experiments/numaug_experiment/run_exp.sh
+++ experiments/numaug_experiment/run_exp.sh
@@ -1,9 +1,9 @@
-python3 reproduce_fig2/train_eval.py --seed 0
+python3 experiments/numaug_experiment/train_eval.py --seed 0
 
-python3 reproduce_fig2/train_eval.py --seed 1
+python3 experiments/numaug_experiment/train_eval.py --seed 1
 
-python3 reproduce_fig2/train_eval.py --seed 2
+python3 experiments/numaug_experiment/train_eval.py --seed 2
 
-python3 reproduce_fig2/train_eval.py --seed 3
+python3 experiments/numaug_experiment/train_eval.py --seed 3
 
-python3 reproduce_fig2/train_eval.py --seed 4
+python3 experiments/numaug_experiment/train_eval.py --seed 4
