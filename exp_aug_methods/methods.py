4d50eca7da96fed3f571ddea58216e6e214c0b2c
nikhilkandukuri <nikhilkand@gmail.com>
2023-11-27 21:03:03 -0500

    Duplicate code

--- exp_aug_methods/methods.py
+++ exp_aug_methods/methods.py
@@ -178,22 +178,6 @@ def gen_punc_aug(train_orig, output_file, num_aug=9):
 		aeda.writelines(data_aug)
 	print("finished punc_aug for", train_orig, "to", output_file)
 
-def gen_hybrid_aeda_aug(train_orig, output_file, num_aug=9):
-	lines = open(train_orig, 'r').readlines()
-	data_aug = []
-	for line in lines:
-		line1 = line.split('\t')
-		label = line1[0]
-		sentence = line1[1]
-		sentence_aug = aeda_3(sentence,num_aug)
-		for i in range(len(sentence_aug)):
-			line_aug = '\t'.join([label, sentence_aug[i]])
-			data_aug.append(line_aug)
-	with open(output_file, 'w') as aeda:
-		aeda.writelines(data_aug)
-	print("finished hybrid aeda for", train_orig, "to", output_file)
-
-
 ###################################################
 ############### Adding Numbers #################
 ###################################################
