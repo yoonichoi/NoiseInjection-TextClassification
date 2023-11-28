e20915d0298de6bfec22f249d705e74a38dd220f
yoonichoi <yooni.jy.choi@gmail.com>
2023-11-27 22:48:55 -0500

    add numaug experiment folder

---
+++ experiments/numaug_experiment/data_process.py
@@ -0,0 +1,36 @@
+from methods import *
+from config import *
+
+if __name__ == "__main__":
+
+	#generate the augmented data sets
+	for dataset_folder in dataset_folders:
+		#pre-existing file locations
+		train_orig = dataset_folder + '/train_orig.txt'
+		
+		for numaug in num_augs:
+			# EDA augmentation
+			train_eda = f"{dataset_folder}/train_eda_{str(numaug)}.txt"
+			gen_eda_aug(train_orig, train_eda, numaug)	# creates train+eda
+
+			# AEDA augmentation
+			train_aeda = f"{dataset_folder}/train_aeda_{str(numaug)}.txt"
+			gen_punc_aug(train_orig, train_aeda, numaug)	# creates train+aeda
+
+			# NUM augmentation (A4)
+			train_num = f"{dataset_folder}/train_num_{str(numaug)}.txt"
+			gen_num_aug(train_orig, train_num, numaug)	# creates train+num
+
+			# APLHA augmentation (A4)
+			train_alpha = f"{dataset_folder}/train_alpha_{str(numaug)}.txt"
+			gen_alpha_aug(train_orig, train_alpha, numaug)	# creates train+alpha
+
+			# HYBRID augmentation (A4)
+			train_hybrid = f"{dataset_folder}/train_hybrid_{str(numaug)}.txt"
+			gen_hybrid_noise_aug(train_orig, train_hybrid, numaug)	# creates train+hybrid
+
+
+
+		#generate the vocab dictionary
+		word2vec_pickle = dataset_folder + '/word2vec.pkl' # don't want to load the huge pickle every time, so just save the words that are actually used into a smaller dictionary
+		gen_vocab_dicts(dataset_folder, word2vec_pickle, huge_word2vec)
---
+++ experiments/numaug_experiment/train_eval.py
@@ -0,0 +1,155 @@
+
+from config import * 
+from methods import *
+import random
+import pickle
+import argparse
+import os
+import shutil
+import gc
+
+###############################
+#### run model and get acc ####
+###############################
+
+def run_model(train_file, test_file, num_classes, percent_dataset, mode=None):
+
+	#initialize model
+	model = build_model(input_size, word2vec_len, num_classes)
+
+	#load data
+	train_x, train_y = get_x_y(train_file, num_classes, word2vec_len, input_size, word2vec, percent_dataset)
+	test_x, test_y = get_x_y(test_file, num_classes, word2vec_len, input_size, word2vec, 1)
+
+	#implement early stopping
+	callbacks = [EarlyStopping(monitor='val_loss', patience=3)]
+
+	#train model
+	model.fit(	train_x, 
+				train_y, 
+				epochs=100000, 
+				callbacks=callbacks,
+				validation_split=0.1, 
+				batch_size=1024, 
+				shuffle=True, 
+				verbose=0)
+	#model.save('checkpoints/lol')
+	#model = load_model('checkpoints/lol')
+
+	#evaluate model
+	y_pred = model.predict(test_x)
+	test_y_cat = one_hot_to_categorical(test_y)
+	y_pred_cat = one_hot_to_categorical(y_pred)
+	acc = accuracy_score(test_y_cat, y_pred_cat)
+
+	if mode is not None:
+		basepath, dataset, aug = mode
+		analyze_path = os.path.join(basepath, 'analyze_result', dataset, str(increment), aug)
+		os.makedirs(analyze_path, exist_ok=True)
+		save_pickle(analyze_path + '/y_pred.pkl', y_pred)
+		save_pickle(analyze_path + '/test_x.pkl', test_x)
+		save_pickle(analyze_path + '/test_y.pkl', test_y)
+		print("pickle files saved to", analyze_path)
+
+	#clean memory???
+	train_x, train_y = None, None
+	gc.collect()
+
+	#return the accuracy
+	#print("data with shape:", train_x.shape, train_y.shape, 'train=', train_file, 'test=', test_file, 'with fraction', percent_dataset, 'had acc', acc)
+	return acc
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--seed', type=int, default=0)
+	parser.add_argument('--runname', type=str)
+	parser.add_argument('--analyze', action='store_true')
+	args = parser.parse_args()
+
+	sd = args.seed
+	runname = args.runname
+	analyze = args.analyze
+
+	analyze_mode = True if analyze else False
+	print("analyze mode:", analyze_mode)
+	random.seed(sd)
+
+	#get the accuracy at each addratio
+	# orig_accs = {dataset:{} for dataset in datasets}
+	eda_accs = {dataset:{} for dataset in datasets}
+	aeda_accs = {dataset:{} for dataset in datasets}
+	num_accs = {dataset:{} for dataset in datasets} # (A4)
+	alpha_accs = {dataset:{} for dataset in datasets} # (A4)
+	hybrid_accs = {dataset:{} for dataset in datasets} # (A4)
+
+	# make folder named with timenow_results
+	nowstr = get_now_str() if runname is None else runname
+	basepath = f'experiments/numaug_experiment/outputs/{nowstr}'   # (A4)
+	os.mkdir(basepath)
+	
+
+	writer = open(f'{basepath}/result_{sd}.csv', 'w')
+
+	#for each dataset
+	for i, dataset_folder in enumerate(dataset_folders):
+		writer.write(dataset + ', numaug, eda_acc, aeda_acc, num_acc, alpha_acc, hybrid_acc' + '\n')
+
+		dataset = datasets[i]
+		num_classes = num_classes_list[i]
+		input_size = input_size_list[i]
+
+		train_orig = dataset_folder + '/train_orig.txt'
+		test_path = dataset_folder + '/test.txt'
+		word2vec_pickle = dataset_folder + '/word2vec.pkl'
+		word2vec = load_pickle(word2vec_pickle)
+
+		increment = 1	# full data
+
+		#calculate original accuracy
+		orig_acc = run_model(train_orig, test_path, num_classes, 1, mode=(basepath, dataset,'orig') if analyze_mode else None)
+		print(dataset, "orig_acc:", orig_acc)
+		writer.write(f"{dataset}, orig_acc: {orig_acc}\n")
+		writer.flush()
+
+		for numaug in num_augs:
+
+			train_eda = f"{dataset_folder}/train_eda_{str(numaug)}.txt"
+			train_aeda = f"{dataset_folder}/train_aeda_{str(numaug)}.txt"
+			train_num = f"{dataset_folder}/train_num_{str(numaug)}.txt"
+			train_alpha = f"{dataset_folder}/train_alpha_{str(numaug)}.txt"
+			train_hybrid = f"{dataset_folder}/train_hybrid_{str(numaug)}.txt"
+			
+			#calculate num accuracy (A4)
+			num_acc = run_model(train_num, test_path, num_classes, increment, mode=(basepath, dataset,'num') if analyze_mode else None)
+			num_accs[dataset][numaug] = num_acc
+
+			#calculate alpha accuracy (A4)
+			alpha_acc = run_model(train_alpha, test_path, num_classes, increment, mode=(basepath, dataset,'alpha') if analyze_mode else None)
+			alpha_accs[dataset][numaug] = alpha_acc
+
+			#calculate hybrid accuracy (A4)
+			hybrid_acc = run_model(train_hybrid, test_path, num_classes, increment, mode=(basepath, dataset,'hybrid') if analyze_mode else None)
+			hybrid_accs[dataset][numaug] = hybrid_acc
+			
+			#calculate eda accuracy
+			eda_acc = run_model(train_eda, test_path, num_classes, increment, mode=(basepath, dataset,'eda') if analyze_mode else None)
+			eda_accs[dataset][numaug] = eda_acc
+
+			#calculate aeda accuracy
+			aeda_acc = run_model(train_aeda, test_path, num_classes, increment, mode=(basepath, dataset,'aeda') if analyze_mode else None)
+			aeda_accs[dataset][numaug] = aeda_acc
+
+			print(dataset, numaug, eda_acc, aeda_acc, num_acc, alpha_acc, hybrid_acc)  # (A4)
+			writer.write(dataset + ',' + str(numaug) + ',' + str(eda_acc) + ',' + str(aeda_acc) + str(num_acc) + str(alpha_acc) + str(hybrid_acc)+'\n') # (A4)
+			writer.flush()
+
+			gc.collect()
+		writer.write('\n')
+
+	if analyze_mode:
+		zip_dir = f'{basepath}/analyze_result'
+		shutil.make_archive(f'{zip_dir}/data', 'zip', zip_dir)
+		print(f"Zip file for result analysis created successfully.")
+
+	print(orig_acc)
+	print(eda_accs, aeda_accs, num_accs, alpha_accs, hybrid_accs)
